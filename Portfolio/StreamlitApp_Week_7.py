ca2ef6ee2a0ceb57640914f8750981a2e67ee26e
an-ninh-nguyen <ninhan80299@gmail.com>
2026-03-24 13:35:28 -0500

    Update StreamlitApp_Week_7.py

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -59,7 +59,7 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["MPWR", "AAPL"],
+        "keys": ["GOOG", "ADI"],
         "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
 }
 
