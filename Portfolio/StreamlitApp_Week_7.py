073bf8b53ed8de10836aa030159bf7aed69c5b90
an-ninh-nguyen <ninhan80299@gmail.com>
2026-03-24 13:46:03 -0500

    Update StreamlitApp_Week_7.py

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -60,7 +60,7 @@ MODEL_INFO = {
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
         "keys": ["GOOG", "ADI"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["GOOG", "ADI"]]
 }
 
 def load_pipeline(_session, bucket, key):
