0646f0fed23e41af9afdeec29a11a0a59ffce4db
An Nguyen <ninhan80299@gmail.com>
2026-04-23 18:12:28 -0500

    Update StreamlitApp_Project.py

--- Portfolio/StreamlitApp_Project.py
+++ Portfolio/StreamlitApp_Project.py
@@ -135,7 +135,7 @@ def display_explanation(input_df, session, aws_bucket):
     input_df=pd.DataFrame(input_df)
     input_df_transformed = preprocessing_pipeline.transform(input_df)
     #feature_names = best_pipeline[:-3].get_feature_names_out()
-    feature_names = dataset.columns
+    feature_names = list(dataset.columns[1:])
     selector = best_pipeline.named_steps['selector']
     selected_features = feature_names[selector.get_support()]
     input_df_transformed = pd.DataFrame(input_df_transformed, columns=selected_features)
